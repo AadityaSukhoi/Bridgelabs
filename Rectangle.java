d5de97f2ce65510895497efa982a34cab3e09b9c
AadityaSukhoi <aadityaranjanmoitra@gmail.com>
2025-08-06 17:29:32 +0530

    Fixed Rectangle.java to print a hollow rectangle.

--- Rectangle.java
+++ Rectangle.java
@@ -1,17 +1,22 @@
-
 import java.util.Scanner;
 
 public class Rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the height of the diamond: ");
-        int height = sc.nextInt();
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= height - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= 2 * i - 1; k++) {
-                System.out.print("*");
+
+        System.out.print("Enter the length of the rectangle: ");
+        int length = sc.nextInt();
+
+        System.out.print("Enter the width of the rectangle: ");
+        int width = sc.nextInt();
+        
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                if (i == 0 || i == length - 1 || j == 0 || j == width - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
