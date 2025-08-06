3ad4455feb6c3d473d1a055641b93e53cda518cd
AadityaSukhoi <aadityaranjanmoitra@gmail.com>
2025-08-06 16:30:56 +0530

    Java Pattern Codes

---
+++ diamond.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of the diamond: ");
+        int height = sc.nextInt();
+        int n = height / 2;
+    }
+}
